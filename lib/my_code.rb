c30a66b96725d22d417b094a202fe5ffa21d218c
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2020-01-01 12:24:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+#  index = 0 
+#  while index = 0 do
+#    source_array[0]
+#    index += 1
+#  end
+#  index
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  index = 0 
+  doubled_array = []
+  while index = 0 do
+    doubled_array << source_array[index]*2
+    index += 1
+  end
+  index
+end
+
+def map_to_square(source_array)
+end
+
+def reduce_to_total(source_array, starting_point)
+end
+
+def reduce_to_all_true(source_array)
+end
+
+def reduce_to_any_true(source_array)
+end
